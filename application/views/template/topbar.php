2da4a027eaad8f8cf2af204f9fc3153501cd4fdd
Al Riefqy <ekydesmito@gmail.com>
2021-02-25 22:08:37 +0800

    remove message icon

--- application/views/template/topbar.php
+++ application/views/template/topbar.php
@@ -102,7 +102,7 @@
                 <li class="nav-item dropdown no-arrow mx-1">
 <!--                    <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
 <!--                        <i class="fas fa-envelope fa-fw"></i>-->
-<!--                        <!-- Counter - Messages -->-->
+<!--                         Counter - Messages -->
 <!--                        <span class="badge badge-danger badge-counter">7</span>-->
 <!--                    </a>-->
                     <!-- Dropdown - Messages -->
